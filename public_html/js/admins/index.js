989c59c9ac814b13abebba873b4dc4ef5f203882
Johnny Le <johnnyxdblack@gmail.com>
2019-05-02 04:15:43 +0100

    Final

---
+++ public_html/js/admins/index.js
@@ -0,0 +1,11 @@
+
+
+// Display confirmation dialog for approving action
+function confirmApprove(){
+  return confirm('Are you sure you want to approve this?');
+}
+
+// Display confirmation dialog for approving action
+function confirmReject(){
+  return confirm('Are you sure you want to reject this?');
+}
